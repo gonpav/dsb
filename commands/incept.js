b9f2f91c37d816104012328132c902a349d27967
pavel <gonpav@gmail.com>
2023-03-24 15:39:14 +0800

    Check 'Admin Inceptor' role before start

--- commands/incept.js
+++ commands/incept.js
@@ -9,6 +9,13 @@ module.exports = {
 	async execute(interaction) {
 
 		// Check for Admin permissions
+		if (!(interaction.member.roles.cache.some(role => role.name === discord_admin_inceptor_role_name))) {
+			return await interaction.reply({
+				content: `You should be a member of '${discord_admin_inceptor_role_name}' role to execute this slash command`,
+				ephemeral: true });
+		}
+
+		await interaction.reply({ content: 'All good', ephemeral: true });
 
 		// interaction.guild is the object representing the Guild in which the command was run
 		// await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
