46ecc1d5475106f270bc3b8f6692cf486aa3a380
pavel <gonpav@gmail.com>
2023-03-30 10:24:30 +0800

    Discord delete all Guild commands script

---
+++ delete-all-commands.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-multi-spaces */
+/* eslint-disable no-inline-comments */
+
+// https://discordjs.guide/creating-your-bot/command-deployment.html#command-registration
+
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+// for guild-based commands
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
+	.catch(console.error);
+
+// for global commands - outcomment below:
+// rest.put(Routes.applicationCommands(clientId), { body: [] })
+// 	.then(() => console.log('Successfully deleted all application commands.'))
+// 	.catch(console.error);
