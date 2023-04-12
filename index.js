b4d97bffc49b2c604fbf245c392e4206a0552328
gonpav <>
2023-04-12 05:35:56 +0000

    Running on replit added ( with keeping alive)

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-// Require the necessary discord.js classes
+// Require keep_alive.js to continue working in Replit env:
+// https://docs.replit.com/tutorials/nodejs/build-basic-discord-bot-nodejs#keeping-our-bot-alive
+const keep_alive = require('./keep_alive.js');
+
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
 // Create a new client instance
 // const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const client = new Client({ intents: [
+const client = new Client({
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers, // Required to show Welcome to new memebers
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMessageReactions] });
+    GatewayIntentBits.GuildMessageReactions]
+});
 
 const readClientCommands = function() {
-    client.commands = new Collection();
+  client.commands = new Collection();
 
-    const commandsPath = path.join(__dirname, 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  const commandsPath = path.join(__dirname, 'commands');
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        }
-        else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
     }
+    else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+  }
 };
 readClientCommands();
 
 const readClientEvents = function() {
-    const eventsPath = path.join(__dirname, 'events');
-    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+  const eventsPath = path.join(__dirname, 'events');
+  const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-    for (const file of eventFiles) {
-        const filePath = path.join(eventsPath, file);
-        const event = require(filePath);
-        if (event.once) {
-            client.once(event.name, (...args) => event.execute(...args));
-        }
-        else {
-            client.on(event.name, (...args) => event.execute(...args));
-        }
+  for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(...args));
     }
+    else {
+      client.on(event.name, (...args) => event.execute(...args));
+    }
+  }
 };
 readClientEvents();
 
